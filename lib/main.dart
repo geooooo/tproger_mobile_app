ea5a696c1d305b4997b899fcd83acb93023c1a34
geooooo <geo97it@gmail.com>
2022-01-13 21:22:51 +0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => const App();
+
+class App extends StatelessWidget {
+  const App({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Text('+');
+}
