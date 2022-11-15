e45a67a2fcd86f8d3e9a704331c3278517c49dfe
geooooo <geo97it@gmail.com>
2022-11-16 01:05:33 +0300

    fix bug with keep alive list

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,7 @@ import 'package:tproger_mobile_app/src/services/di_configuration/configure_injec
 import 'package:tproger_mobile_app/src/services/store_factory.dart';
 import 'package:tproger_mobile_app/src/widgets/app_widget.dart';
 
-// TODO: fonts
 // TODO: fix blur
-// TODO: bug
-// Performing hot restart...
-// Restarted application in 355ms.
-// [VERBOSE-2:dart_vm_initializer.cc(41)] Unhandled Exception: setState() called after dispose(): _ArticleContentImageContainerWidgetState#ece6c(lifecycle state: defunct, not mounted)
-// This error happens if you call setState() on a State object for a widget that no longer appears in the widget tree (e.g., whose parent widget no longer includes the widget in its build). This error can occur when code calls setState() from a timer or an animation callback.
-// The preferred solution is to cancel the timer or stop listening to the animation in the dispose() callback. Another solution is to check the "mounted" property of this object before calling setState() to ensure the object is still in the tree.
-// This error might indicate a memory leak if setState() is being called because another object is retaining a reference to this State object after it has been removed from the tree. To avoid memory leaks, consider breaking the reference to this object during dispose().
-// #0      State.setState.<anonymous closure> (pack<…>
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/src/widgets/article_list_page/article_list/article_widget/article_widget.dart
+++ lib/src/widgets/article_list_page/article_list/article_widget/article_widget.dart
@@ -6,10 +6,8 @@ import 'package:tproger_mobile_app/src/models/article_model.dart';
 import 'package:tproger_mobile_app/src/widgets/article_list_page/article_list/article_widget/article_background_image_widget.dart';
 import 'package:tproger_mobile_app/src/widgets/article_list_page/article_list/article_widget/article_body_widget.dart';
 
-class ArticleWidget extends StatelessWidget {
+class ArticleWidget extends StatefulWidget {
   final ArticleModel article;
-
-  bool get _hasBackgroundImage => article.image is ArticleBackgroundImage;
 
   const ArticleWidget({
     required this.article,
@@ -17,17 +15,31 @@ class ArticleWidget extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => StoreBuilder<AppState>(
-    builder: (builder, store) => DecoratedBox(
-      decoration: BoxDecoration(
-        color: store.state.theme.articleBackgroundColor,
+  State<ArticleWidget> createState() => _ArticleWidgetState();
+}
+
+class _ArticleWidgetState extends State<ArticleWidget> with AutomaticKeepAliveClientMixin {
+  bool get _hasBackgroundImage => widget.article.image is ArticleBackgroundImage;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    
+    return StoreBuilder<AppState>(
+      builder: (builder, store) => DecoratedBox(
+        decoration: BoxDecoration(
+          color: store.state.theme.articleBackgroundColor,
+        ),
+        child: _hasBackgroundImage
+          ? ArticleBackgroundImageWidget(
+              imageLink: widget.article.image!.link,
+              child: ArticleBodyWidget(article: widget.article),
+            )
+          : ArticleBodyWidget(article: widget.article),
       ),
-      child: _hasBackgroundImage
-        ? ArticleBackgroundImageWidget(
-            imageLink: article.image!.link,
-            child: ArticleBodyWidget(article: article),
-          )
-        : ArticleBodyWidget(article: article),
-    ),
-  );
+    );
+  }
 }
