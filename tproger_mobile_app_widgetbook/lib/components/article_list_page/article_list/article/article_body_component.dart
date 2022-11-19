5fcd12eb921a728876cb75ebcbc0a5cc47231e58
geooooo <geo97it@gmail.com>
2022-11-19 18:52:36 +0300

    update widgetbook

---
+++ tproger_mobile_app_widgetbook/lib/components/article_list_page/article_list/article/article_body_component.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:tproger_mobile_app_widgetbook/services/helper_service.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:tproger_mobile_app/src/widgets/article_list_page/article_list/article/article_body_widget.dart';
+import 'package:tproger_mobile_app/src/models/enums/reaction.dart';
+
+WidgetbookComponent buildArticleBodyComponent(BuildContext context) => WidgetbookComponent(
+  name: 'ArticleBody',
+  useCases: [
+    WidgetbookUseCase(
+      name: 'Default', 
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ArticleBodyWidget(
+            article: HelperService.createArticleModel(isBackgroundImage: false),
+          ),
+        ],
+      ),
+    ),
+    WidgetbookUseCase(
+      name: 'Inverted', 
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ArticleBodyWidget(
+            article: HelperService.createArticleModel(isBackgroundImage: true),
+          ),
+        ],
+      ),
+    ),
+    WidgetbookUseCase(
+      name: 'Custom', 
+      builder: (context) {
+        final angryReactionCount = context.knobs.number(
+          label: 'Count of angry reactions',
+          initialValue: 0,
+        ).toInt();
+        final surpirseReactionCount = context.knobs.number(
+          label: 'Count of surprise reactions',
+          initialValue: 0,
+        ).toInt();
+        final thinkingReactionCount = context.knobs.number(
+          label: 'Count of thinking reactions',
+          initialValue: 0,
+        ).toInt();
+        final laughReactionCount = context.knobs.number(
+          label: 'Count of laugh reactions',
+          initialValue: 0,
+        ).toInt();
+        final likeReactionCount = context.knobs.number(
+          label: 'Count of like reactions',
+          initialValue: 0,
+        ).toInt();
+        final reactionToCounts = {
+          if (angryReactionCount > 0) Reaction.angry: angryReactionCount, 
+          if (surpirseReactionCount > 0) Reaction.surprise: surpirseReactionCount, 
+          if (thinkingReactionCount > 0) Reaction.thinking: thinkingReactionCount, 
+          if (laughReactionCount > 0) Reaction.laugh: laughReactionCount, 
+          if (likeReactionCount > 0) Reaction.like: likeReactionCount, 
+        };
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ArticleBodyWidget(
+              article: HelperService.createArticleModel(
+                isBackgroundImage: context.knobs.boolean(
+                  label: 'Has background image if true else icon',
+                  initialValue: false,
+                ),
+                backgroundColor: context.knobs.text(
+                  initialValue: '#000000',
+                  label: 'Color of a background',
+                  description: 'Only if isBackgroundImage is false'
+                ),
+                authorName: context.knobs.text(
+                  label: 'Name',
+                  initialValue: 'Author Name',
+                ),
+                title: context.knobs.text(
+                  label: 'Title',
+                  initialValue: 'Title of an article',
+                ),
+                description: context.knobs.text(
+                  label: 'Description',
+                  initialValue: 'Description of an article',
+                ),
+                viewCount: context.knobs.number(
+                  label: 'Count of views',
+                  initialValue: 0,
+                ).toInt(),
+                bookmarkCount: context.knobs.number(
+                  label: 'Count of bookmarks',
+                  initialValue: 0,
+                ).toInt(),
+                commentCount: context.knobs.number(
+                  label: 'Count of comments',
+                  initialValue: 0,
+                ).toInt(),
+                reactionToCounts: reactionToCounts,
+              ),
+            ),
+          ],
+        );
+      },
+    ),
+  ],
+);
--- tproger_mobile_app_widgetbook/lib/folders/article_folder.dart
+++ tproger_mobile_app_widgetbook/lib/folders/article_folder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tproger_mobile_app_widgetbook/components/article_list_page/article_list/article/article_body_component.dart';
 import 'package:tproger_mobile_app_widgetbook/components/article_list_page/article_list/article/article_component.dart';
 import 'package:widgetbook/widgetbook.dart';
 
@@ -6,6 +7,7 @@ WidgetbookFolder buildArticleFolder(BuildContext context) => WidgetbookFolder(
   name: 'Article',
   widgets: [
     buildArticleComponent(context),
+    buildArticleBodyComponent(context),
   ],
   folders: [
     
